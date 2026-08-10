aeae9bfe6eb660b3bcd35af3b80524bfed236543
Narabadee <narabadeebb29@gmail.com>
2026-08-09 23:23:20 -0700

    feat: add User model and connect auth-service to MongoDB

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+mongoose.connect("mongodb://localhost:27017/authdb");
 app.get("/", (req, res) => {res.send("Hello from Auth Service!");
     });
     app.listen(4001, () => {
